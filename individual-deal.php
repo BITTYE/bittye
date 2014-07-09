5a81b684c0b8cd565f4031e32463c6331d483d0f
graphicana <pavan_sry@yahoo.co.in>
2014-07-10 00:08:56 +0530

    new individual deals design with more new and useful data fields

--- individual-deal.php
+++ individual-deal.php
@@ -1,14 +1,36 @@
-<div class="face">
-    <div class="col-md-8">
-        <div class="place">Bangalore</div>
-        <div class="title">Title of the deal</div>
-        <div class="smalDisc">We serve the best masala dosa and delicious rice in the city</div>
-    </div>
-    <div class="col-md-4">
-        <div class="value">&#8377;1234</div>
-        <a class="grabIt" href="">Grab it!</a>
-    </div>
-    <div class="col-md-12">
-        <img src="images/ad.jpg" alt=""/>
+<div class="ind-deal">
+    <div class="artist-data pull-left">
+        <div class="artst-pic pull-left">
+            <span class="artst-like"><a href="#"><i class="glyphicon glyphicon-heart-empty"></i></a></span>
+            <span class="artst-share"><a href="#"><i class="glyphicon glyphicon-share"></i></a></span>
+            <span class="artst-plus"><a href="#"><i class="glyphicon glyphicon-plus-sign"></i></a></span>
+            <a href="#">
+                <img src="http://placehold.it/122x122" alt="" class="img-responsive"/>
+            </a>
+        </div>
+        <div class="artst-prfle pull-right">
+            <div class="art-title">
+                Lorem ipsum dolor
+                <span class="artst-sub">By <span class="byname">Lorem Ipsum</span> <span
+                        class="daysago">5 Days Ago</span></span>
+            </div>
+            <div class="counter-tab">
+                <div class="counter_comnt"><i class="glyphicon glyphicon-comment"></i> 359</div>
+                <div class="counter_ply"><i class="glyphicon glyphicon-play"></i> 1312</div>
+                <div class="counter_like"><i class="glyphicon glyphicon-heart"></i> 2256</div>
+            </div>
+        </div>
+        <div class="bot-links">
+            <a href="#">Sit amet</a>
+            <a href="#">Amet</a>
+            <a href="#">Lorem</a>
+            <a href="#">Ipsum dolor</a>
+            <a href="#">Lorem</a>
+            <a href="#">Amet</a>
+            <a href="#">Sit amet</a>
+            <a href="#">Ipsum dolor</a>
+            <a href="#">Lorem</a>
+            <a href="#">Ipsum dolor sit</a>
+        </div>
     </div>
 </div>
