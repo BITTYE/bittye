853d67bcb82d1c27d93cdb363a3ec30d1b073410
graphicana <pavan_sry@yahoo.co.in>
2014-07-10 01:03:37 +0530

    added user profile snippet

--- header.php
+++ header.php
@@ -17,6 +17,24 @@
                         <!-- /.nav-collapse -->
                     </div>
                     <!-- /.container-fluid -->
+                    <div class="user-profile">
+                        <ul class="nav navbar-nav">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">User Profile<span class="glyphicon glyphicon-user pull-right"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Account Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Favourites Snippets <span class="glyphicon glyphicon-heart pull-right"></span></a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Sign Out <span class="glyphicon glyphicon-log-out pull-right"></span></a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
                     <div class="collapse navbar-collapse bs-example-js-navbar-collapse pull-right">
                         <div class="dropdown">
                             <a data-toggle="dropdown" href="#">Login</a>
