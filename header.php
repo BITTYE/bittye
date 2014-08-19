a0d21d419b455543a64749055c420dfa56aa4e47
graphicana <pavan_sry@yahoo.co.in>
2014-08-19 09:58:37 +0530

    creating modules

--- header.php
+++ header.php
@@ -53,29 +53,7 @@
                     <h3 class="rs title-mod">Hello pretty! Welcome to bittye.com</h3>
                     <div class="wrap-form-reg clearfix">
                         <form action="#">
-                            <div class="left-form">
-                                <label class="wrap-txt" for="sys_email">
-                                    <input class="input-txt" id="sys_email" type="email" placeholder="you@mail.com">
-                                </label>
-                                <label class="wrap-txt" for="sys_pass">
-                                    <input class="input-txt" id="sys_pass" type="password" placeholder="password please!">
-                                </label>
-                                <label class="wrap-check" for="sys_chk_news">
-                                    <input id="sys_chk_news" class="input-chk" type="checkbox"> Remember me
-                                    <i class="icon iUncheck"></i>
-                                    <a class="lost-pass" href="#">Forgot password ?</a>
-                                </label>
-                                <div class="wrap-login-btn">
-                                    <button class="btn-flat gr btn-submit-reg" type="submit">Login</button>
-                                    <div class="sep-connect">
-                                        <span>Or</span>
-                                    </div>
-                                    <div class="grp-connect">
-                                        <a class="btn-flat fb" href="#">Facebook</a>
-                                        <a class="btn-flat gg" href="#">Google</a>
-                                    </div>
-                                </div>
-                            </div>
+                            <?php include 'login-form.php';?>
                             <div class="right-create-acc">
                                 <img class="account" src="images/reg-account.png" alt="bittye.com">
                                 <p class="lbl-dung-lo rs">Not a member? Don’t worry</p>
@@ -91,28 +69,7 @@
                     <div class="desc-reg">Sign up for free and get exclusive access to members-only savings, rewards and special promotions from Coupons.com. Enter in an email and a password or sign up with Facebook.</div>
                     <div class="wrap-form-reg clearfix">
                         <form action="#">
-                            <div class="left-form">
-                                <label class="wrap-txt" for="sys_email_reg">
-                                    <input class="input-txt" id="sys_email_reg" type="email" placeholder="you@mail.com"/>
-                                </label>
-                                <label class="wrap-txt" for="sys_pass_reg">
-                                    <input class="input-txt" id="sys_pass_reg" type="password" placeholder="password please!"/>
-                                </label>
-                                <label class="wrap-txt" for="sys_pass_reg">
-                                    <input class="input-txt" id="sys_pass_conf" type="password" placeholder="confirm password please!"/>
-                                </label>
-                                <label class="wrap-txt" for="sys_pass_reg">
-                                    <input class="input-txt" id="mobile_reg" type="password" placeholder="mobile number please!"/>
-                                </label>
-                                <label class="wrap-check" for="sys_chk_news_reg">
-                                    <input id="sys_chk_news_reg" class="input-chk" type="checkbox"/> Send me the weekly bittye.com’s offers.
-                                    <i class="icon iUncheck"></i>
-                                </label>
-                                <label class="wrap-check" for="sys_chk_agree">
-                                    <input id="sys_chk_agree" class="input-chk" type="checkbox"/> I agree to the <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.
-                                    <i class="icon iUncheck"></i>
-                                </label>
-                            </div>
+                            <?php include 'register-form.php';?>
                             <div class="right-connect">
                                 <button class="btn-flat yellow btn-submit-reg" type="submit">Create an account</button>
                                 <div class="sep-connect">
---
+++ register-form.php
@@ -0,0 +1,16 @@
+<div class="left-form">
+    <label class="wrap-txt" for="sys_email">
+        <input class="input-txt" id="sys_email" type="email" placeholder="you@mail.com"/>
+    </label>
+    <label class="wrap-txt" for="sys_pass">
+        <input class="input-txt" id="sys_pass" type="password" placeholder="password please!"/>
+    </label>
+    <label class="wrap-check" for="sys_chk_news">
+        <input id="sys_chk_news" class="input-chk" type="checkbox"/> Send me the weekly Couponday.com’s offers.
+        <i class="icon iUncheck"></i>
+    </label>
+    <label class="wrap-check" for="sys_chk_agree">
+        <input id="sys_chk_agree" class="input-chk" type="checkbox"/> I agree to the <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.
+        <i class="icon iUncheck"></i>
+    </label>
+</div>
